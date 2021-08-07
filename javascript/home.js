efd5a6e2480c11b472c659dade9c765c2af0d680
Mcsquar <87913289+Mcsquar@users.noreply.github.com>
2021-08-07 15:14:10 -0400

    Update home.js

--- javascript/home.js
+++ javascript/home.js
@@ -23,7 +23,7 @@ document.querySelector(".month").innerHTML = month;
 var year =  today.getFullYear();
 document.querySelector(".month").innerHTML = month + " - " +year;}
 function fadeout(){
-document.getElementByClass("Welcome").addClass("slide-out-left")
+document.getElementByClassName("Welcome").addClass("slide-out-left")
 }
 function fadeoutdelay(){
 setTimeout(fadeout,5000)
